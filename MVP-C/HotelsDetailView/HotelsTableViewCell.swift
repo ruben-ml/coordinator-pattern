9980576196806ed6198470650d0b0396d44295e7
Rubén Muñoz López <>
2021-09-21 17:53:47 +0200

    Configure cell for tableView

---
+++ MVP-C/HotelsDetailView/HotelsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HotelsTableViewCell.swift
+//  MVP-C
+//
+//  Created by Rubén Muñoz López on 21/9/21.
+//
+
+import UIKit
+
+class HotelsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
