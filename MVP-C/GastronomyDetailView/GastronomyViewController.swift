5b6ca87cc761d88d4b02cc957268c7038a56dd0d
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2021-10-14 09:27:18 +0200

    Working programatically auto layout

--- MVP-C/GastronomyDetailView/GastronomyViewController.swift
+++ MVP-C/GastronomyDetailView/GastronomyViewController.swift
@@ -10,28 +10,38 @@ import UIKit
 public class GastronomyViewController: UIViewController, Storyboarded {
     
     private lazy var scrollView: UIScrollView = {
-      let scrollView = UIScrollView()
-      scrollView.translatesAutoresizingMaskIntoConstraints = false
-      return scrollView
-    }()
-
-    private lazy var contentView: UIView = {
-      let contentView = UIView()
-      contentView.backgroundColor = UIColor.red
-      contentView.translatesAutoresizingMaskIntoConstraints = false
-      return contentView
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = UIColor.white
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
     }()
     
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.backgroundColor = UIColor.red
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
     
     private lazy var stack: UIView = {
-       let stackView = UIStackView(arrangedSubviews: [titleName, imageMain, titleDrinks, imageDrink, titleDesserts, imageDessert])
+        let stackView = UIStackView(arrangedSubviews: [titleName, imageMain, titleDrinks, imageDrink, titleDesserts, imageDessert, stackMain])
         stackView.axis = .vertical
         stackView.spacing = 10
         stackView.distribution = .fillProportionally
         stackView.backgroundColor = .white
         stackView.frame = view.bounds
         stackView.translatesAutoresizingMaskIntoConstraints = false
-       return stackView
+        return stackView
+    }()
+    
+    private lazy var stackMain: UIView = {
+        let stackView = UIStackView(arrangedSubviews: [titleStack, imageMainDishes])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillProportionally
+        stackView.frame = view.bounds
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     public lazy var titleName: UILabel = {
@@ -64,16 +74,26 @@ public class GastronomyViewController: UIViewController, Storyboarded {
         return label
     }()
     
-     lazy var imageIcon: UIImageView = {
-        let icon = UIImageView(image: UIImage(named: "idiom"))
-        icon.backgroundColor = UIColor.yellow
-        icon.contentMode = .scaleAspectFit
-        icon.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-        icon.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        return icon
+    public lazy var titleStack: UILabel = {
+        let label = UILabel()
+        label.textColor = .orange
+        label.font = UIFont(name: "Helvetica Neue Bold", size: 20)
+        label.textAlignment = .center
+        label.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        return label
     }()
     
     lazy var imageMain: UIImageView = {
+        let icon = UIImageView(image: UIImage(named: "mainDishes"))
+        icon.backgroundColor = UIColor.yellow
+        icon.contentMode = .scaleToFill
+        icon.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        icon.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        return icon
+    }()
+    
+    lazy var imageMainDishes: UIImageView = {
         let icon = UIImageView(image: UIImage(named: "mainDishes"))
         icon.backgroundColor = UIColor.yellow
         icon.contentMode = .scaleToFill
@@ -100,11 +120,19 @@ public class GastronomyViewController: UIViewController, Storyboarded {
         return icon
     }()
     
+    lazy var viewBackground: UIView = {
+        let background = UIView()
+        background.backgroundColor = UIColor.blue
+        background.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        background.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        return background
+    }()
+    
     public override func viewDidLoad() {
         setUp()
         setupConstraints()
     }
-
+    
     func setUp() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
@@ -112,34 +140,35 @@ public class GastronomyViewController: UIViewController, Storyboarded {
         titleName.text = "Platos típicos de estambul"
         titleDrinks.text = "Bebidas turcas"
         titleDesserts.text = "Postres de turquía"
+        titleStack.text = "title stack"
     }
     
     private func setupConstraints() {
-      NSLayoutConstraint.activate([
-        scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
-        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor)
-      ])
-
-      NSLayoutConstraint.activate([
-        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-        contentView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
-        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-        contentView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
-        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-      ])
-
-      NSLayoutConstraint.activate([
-        stack.topAnchor.constraint(equalTo: contentView.topAnchor),
-        stack.rightAnchor.constraint(equalTo: contentView.rightAnchor),
-        stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-        stack.leftAnchor.constraint(equalTo: contentView.leftAnchor)
-      ])
-//
-//      NSLayoutConstraint.activate([
-//        titleName.topAnchor.constraint(equalTo: stack.topAnchor, constant: 10)
-//      ])
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
+        
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stack.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stack.leftAnchor.constraint(equalTo: contentView.leftAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleName.topAnchor.constraint(equalTo: stack.topAnchor, constant: 10),
+        ])
     }
 }
 
