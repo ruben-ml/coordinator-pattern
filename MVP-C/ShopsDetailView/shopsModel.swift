ee4901a1ff5fb74dba88aaa3ea14f4f91816ae56
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2021-10-28 13:00:41 +0200

    Create mock json

---
+++ MVP-C/ShopsDetailView/shopsModel.swift
@@ -0,0 +1,14 @@
+//
+//  shopsModel.swift
+//  MVP-C
+//
+//  Created by Rubén Muñoz López on 28/10/21.
+//
+
+import Foundation
+import UIKit
+
+struct shopsDTO: Decodable {
+    let name: String?
+    let description: String?
+}
